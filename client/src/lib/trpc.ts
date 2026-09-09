e08c398c397546f9a1fa6e91fdd325debd483872
Financeiro <dina@ckmtalents.net>
2026-09-09 18:53:30 -0300

    fix: preservar tipagem existente do cliente trpc

--- client/src/lib/trpc.ts
+++ client/src/lib/trpc.ts
@@ -1,4 +1,4 @@
 import { createTRPCReact } from "@trpc/react-query";
-import type { RootRouter } from "../../../server/rootRouter";
+import type { AppRouter } from "../../../server/routers";
 
-export const trpc = createTRPCReact<RootRouter>();
+export const trpc = createTRPCReact<AppRouter>();
