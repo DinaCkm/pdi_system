dde23abade0014d8d8764a51e75ef0aab0cf7165
Financeiro <dina@ckmtalents.net>
2026-09-10 23:16:30 -0300

    feat: expor router de tentativa da prova UTIC

--- server/rootRouter.ts
+++ server/rootRouter.ts
@@ -1,9 +1,11 @@
 import { appRouter } from "./routers";
 import { avaliacoesRouter } from "./routers/avaliacoes";
+import { provaUticRouter } from "./routers/provaUtic";
 import { mergeRouters, router } from "./_core/customTrpc";
 
 const avaliacoesRootRouter = router({
   avaliacoes: avaliacoesRouter,
+  provaUtic: provaUticRouter,
 });
 
 export const rootRouter = mergeRouters(appRouter, avaliacoesRootRouter);
