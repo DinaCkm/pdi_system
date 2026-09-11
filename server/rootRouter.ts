a00f6c17bdfa7047d541e212f4ee3962f6fb55b2
Financeiro <dina@ckmtalents.net>
2026-09-11 11:58:28 -0300

    feat: expor resultados da prova UTIC para administração

--- server/rootRouter.ts
+++ server/rootRouter.ts
@@ -2,12 +2,14 @@ import { appRouter } from "./routers";
 import { avaliacoesRouter } from "./routers/avaliacoes";
 import { provaUticRouter } from "./routers/provaUtic";
 import { provaUticTesteRouter } from "./routers/provaUticTeste";
+import { provaUticResultadosRouter } from "./routers/provaUticResultados";
 import { mergeRouters, router } from "./_core/customTrpc";
 
 const avaliacoesRootRouter = router({
   avaliacoes: avaliacoesRouter,
   provaUtic: provaUticRouter,
   provaUticTeste: provaUticTesteRouter,
+  provaUticResultados: provaUticResultadosRouter,
 });
 
 export const rootRouter = mergeRouters(appRouter, avaliacoesRootRouter);
